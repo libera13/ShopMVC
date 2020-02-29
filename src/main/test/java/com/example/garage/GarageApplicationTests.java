bb2dbb37b6a5742dcd3e8aa39b4bc83896ae85cc
libera13 <emillibera@gmail.com>
2020-02-29 19:29:28 +0100

    Add responsive

--- src/main/test/java/com/example/garage/GarageApplicationTests.java
+++ src/main/test/java/com/example/garage/GarageApplicationTests.java
@@ -1,17 +1,9 @@
 package com.example.garage;
 
-import com.example.garage.model.Role;
-import com.example.garage.repository.RoleRepository;
-import com.example.garage.repository.RoleRepositoryClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.persistence.PersistenceException;
-
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
